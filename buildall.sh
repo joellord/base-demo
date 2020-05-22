1ea0036364fe0f4338157fde3dabe581f3f0bd55
Joel Lord <joelphy@gmail.com>
2020-05-22 10:15:10 -0400

    Added k8s files

---
+++ buildall.sh
@@ -0,0 +1,21 @@
+export DOCKER_USER=joellord
+export BASE_NAME=demo
+echo "Build and push all containers as $DOCKER_USER/$BASE_NAME*"
+echo "Starting with frontend"
+cd frontend
+docker build -t $DOCKER_USER/$BASE_NAME-front .
+docker push $DOCKER_USER/$BASE_NAME-front
+
+cd ..
+echo "API"
+cd api
+docker build -t $DOCKER_USER/$BASE_NAME-api .
+docker push $DOCKER_USER/$BASE_NAME-api
+
+cd ..
+echo "Crashing service"
+cd crashing
+docker build -t $DOCKER_USER/$BASE_NAME-crash .
+docker push $DOCKER_USER/$BASE_NAME-crash
+
+cd ..
