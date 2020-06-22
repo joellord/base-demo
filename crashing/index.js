82643fad47d061a4b41c165f0704d08658080def
Joel Lord <joelphy@gmail.com>
2020-06-22 15:47:33 -0400

    Changed the crashing API with more options, tweaked the non-root front end

--- crashing/index.js
+++ crashing/index.js
@@ -5,22 +5,49 @@ const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
-const TTC = 60;
+const TTC = process.env.TTC || 60;
+const SLOW_DURATION = process.env.SLOW_DURATION || 4000;
 let timeLeft = TTC;
 
-console.log(`This server will crash in ${TTC} seconds`);
-console.log("Initiating countdown");
-setTimeout(() => {
-  console.log("System will crash now with a random error number");
-  process.exit((Math.random() * 100) + 1);
-}, TTC * 1000);
-
-setInterval(() => {
-  timeLeft--;
-}, 1000);
+if (process.env.AUTOCRASH) {
+  console.log(`This server will crash in ${TTC} seconds`);
+  console.log("Initiating countdown");
+  setTimeout(() => {
+    console.log("System will crash now with a random error number");
+    process.exit((Math.random() * 100) + 1);
+  }, TTC * 1000);  
+  setInterval(() => {
+    timeLeft--;
+  }, 1000);
+}
 
 app.get("/", (req, res) => {
+  let body = {
+    data: "Hello",
+    timestamp: (new Date()).getTime()
+  };
+  if (timeLeft !== TTC) body.timeLeft = timeLeft;
   res.send({data: "Hello", timestamp: (new Date()).getTime(), timeLeft}).status(200);
+});
+
+app.get("/crash", (req, res) => {
+  console.log("System crash requested...");
+  process.exit((Math.random() * 100) + 1);
+});
+
+app.get("/slow", (req, res) => {
+  console.log("This is a slow request");
+  let start = (new Date()).getTime();
+  setTimeout(() => {
+    let end = (new Date()).getTime();
+    console.log("Requested completed, sending response");
+    res.send({
+      data: "Hello",
+      reqStart: start,
+      reqEnd: end,
+      reqDuration: (end - start) / 1000
+    }).status(200);
+  }, SLOW_DURATION);
 });
 
 app.listen(PORT, () => {
--- frontend/start_nginx.sh
+++ frontend/start_nginx.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-for file in /usr/share/nginx/html/js/app.*.js;
+for file in /code/js/app.*.js;
 do
   if [ ! -f $file.tmpl.js ]; then
     cp $file $file.tmpl.js
   fi
-  envsubst '$WORKING_API_URL' < $file.tmpl.js > $file
-  envsubst '$CRASHING_API_URL' < $file.tmpl.js > $file
+  envsubst \$WORKING_API_URL,\$CRASHING_API_URL < $file.tmpl.js > $file
 done
 echo "Starting Nginx"
 nginx -g 'daemon off;'
